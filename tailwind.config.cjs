b907ae5d802807508686d8ffb0abbbfcee143d89
cuingskot76 <afrizapamujo@gmail.com>
2023-02-14 16:56:58 +0700

    full remove package

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,14 +6,3 @@ module.exports = {
   },
   plugins: [],
 };
-
-// const withMT = require("@material-tailwind/react/utils/withMT");
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = withMT({
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// });
